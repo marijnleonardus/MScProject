62586d319fef474d097caf587d3f5ebe6815e16b
Marijn Venderbosch <48382744+MarijnVenderbosch@users.noreply.github.com>
2021-05-28 13:54:52 +0200

    use matploblib, load image, variable names

--- analyzing spots/StackOverflowAnswerWorkingRavelandReshapeEdit.py
+++ analyzing spots/StackOverflowAnswerWorkingRavelandReshapeEdit.py
@@ -4,13 +4,17 @@ Created on Thu May 27 17:20:51 2021
 
 @author: Marijn Venderbosch
 """
-import scipy.optimize as opt
+from scipy import optimize
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Loads image containing spot and ravels in order to fit
+image = np.load('SpotCroppedNormalized.npy')
+image_raveled = np.ravel(image)
+
 def twoDgaussian(X, amplitude, x0, y0, sigma_x, sigma_y):
     # We define the function to fit: a particular example of a 2D gaussian
-    # indepdent variables x,y are passed as a single variable (curve_fit only) 
+    # indepdent variables x,y are passed as a single variable X (curve_fit only 
     # accepts 1D fitting, therefore the result is raveled 
     x, y = X
     x0 = float(x0)
@@ -20,8 +24,8 @@ def twoDgaussian(X, amplitude, x0, y0, sigma_x, sigma_y):
     return I.ravel()
 
 # Amount of data points = pixels of the image
-x_pixels = 6
-y_pixels = 6
+x_pixels = image.shape[0]
+y_pixels = image.shape[1]
 
 # Create x and y indices. Create a grid using np.meshgrid()
 x = np.arange(0, x_pixels, 1)
@@ -31,19 +35,19 @@ x,y = np.meshgrid(x, y)
 # Generate the data using the gaussian function and the generated grid
 data = twoDgaussian((x, y), 1, x_pixels / 2, y_pixels/2, x_pixels / 10, y_pixels / 20)
 
-# plot twoD_Gaussian data generated above
+# Plot twoD_Gaussian data generated above
 plt.figure()
-plt.imshow(data.reshape(x_pixels ,y_pixels ))
+plt.imshow(data.reshape(x_pixels, y_pixels))
 plt.colorbar()
 
-# add some noise to the data and try to fit the data generated beforehand
+# Add some noise to the data and try to fit the data generated beforehand
 initial_guess = (1, x_pixels/2,y_pixels/2, x_pixels / 10, y_pixels / 20)
 
 data_noisy = data + 0.05 * np.random.normal(size = data.shape)
 
-popt, pcov = opt.curve_fit(twoDgaussian, (x, y), data_noisy, p0 = initial_guess)
+fit_parameters, covariance = optimize.curve_fit(twoDgaussian, (x, y), data_noisy, p0 = initial_guess)
 
-data_fitted = twoDgaussian((x, y), *popt)
+data_fitted = twoDgaussian((x, y), *fit_parameters)
 
 fig, ax = plt.subplots(1, 1)
 ax.imshow(data_noisy.reshape(x_pixels, y_pixels), cmap=plt.cm.jet, origin='bottom',
