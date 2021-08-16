e3018f42e7adcfa4c23b7d4fabe2a0e2719fcf74
Marijn Venderbosch <48382744+MarijnVenderbosch@users.noreply.github.com>
2021-08-16 21:14:48 +0200

    minor updates
    correct dipole potential used
    colorbar
    fixed axis labels
    included function to compute the potential

--- dipole trap/optical dipole trap potential.py
+++ dipole trap/optical dipole trap potential.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 """
 Created on Fri Mar 19 14:55:26 2021
-
 @author: Marijn Venderbosch
 """
-
 
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -12,48 +10,53 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
-# Problem is dimensionless
-w_0 = 1
-z_R = 1
-
-# prefactor for later
-factor = 1
-power = 1
-
 # Initialize r and z coordinates, radial and longitudal direction respectively
 nz, nr = (100, 100)
 
+# Amount of waists/rayleigh Ranges to include
 plotrange_r = 3
 plotrange_z = 3
 
+# Amount of points for 2D grid
 spacing_r = 100
 spacing_z = 100
 
 # Create 2D grid
 rv = np.linspace(-plotrange_r, plotrange_r, spacing_r)
 zv = np.linspace(-plotrange_z, plotrange_z, spacing_z)
-
 r, z = np.meshgrid(rv,zv)
 
-# Compute beam width and intensity
-w = w_0 * np.sqrt(1 + z**2/z_R**2)
-intensity = 2 * power * np.exp(-2 * r**2 / w**2) / (np.pi * w**2)
+# Define optical potential function. Modeled as gaussian with rayleight range z_R
+# and waist w_0. 
+def optical_potential(U0, w0, zR, z, r):
+    prefactor = -U0 / (1 + z**2/zR**2)
+    exponent = -2*r**2 / (w0**2*(1 + z**2 / zR**2))
+    return prefactor * np.exp(exponent)
 
-# Trap depth
-U = -factor * intensity
+# Return dimensionless optical potential. Dimensionless because w_i = z_R = U_0 = 1
+potential = optical_potential(1, 1, 1, z, r)
 
 # Plotting
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-surf = ax.plot_surface(r ,z, U, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
-ax.set_xlabel('r')
-ax.set_ylabel('z')
-ax.set_zlabel(r'Trap depth $U(r,z)$')
+fig = plt.figure(figsize = (8,6))
+ax = fig.gca(projection = '3d')
+surf = ax.plot_surface(r ,z, potential,
+                       cmap = cm.viridis, 
+                       linewidth = 0, 
+                       antialiased = False)
+cb = plt.colorbar(surf, 
+                  ax = [ax], 
+                  shrink = 0.4, 
+                  aspect = 5, 
+                  location ='left',
+                  pad = 0)
+ax.set_xlabel(r'$r/w_0$')
+ax.set_ylabel(r'$z/z_R$')
+ax.set_zlabel(r'$U(r,z)/U_0$')
 ax.grid()
 
 # Saving
-plt.savefig('ODTdepth.png', dpi = 1000)
+plt.savefig('ODTdepth.pdf', 
+            bbox_inches='tight',
+            pad_inches = 0,
+            dpi = 500)
 plt.show()
