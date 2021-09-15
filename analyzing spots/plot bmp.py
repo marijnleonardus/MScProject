664d6f45850f5ae43fda0a34ff61a5cd5edb18d0
Marijn Venderbosch <48382744+MarijnVenderbosch@users.noreply.github.com>
2021-09-16 00:49:58 +0200

    Update plot bmp.py
    save result, define cropping range variable

--- analyzing spots/plot bmp.py
+++ analyzing spots/plot bmp.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+#%% variables
+cropping_range = 150
+
 
 #%% load and crop
 img = np.array(Image.open('exports/6x6_calc_output.bmp'))
@@ -21,11 +24,12 @@ def crop_center(img, cropx, cropy):
     starty = y//2 - (cropy//2)    
     return img[starty:starty+cropy,startx:startx+cropx]
 
-img_cropped = crop_center(img, 150, 150)
+img_cropped = crop_center(img, cropping_range, cropping_range)
 
 #%%plot and save
 fig, ax = plt.subplots()
 ax.imshow(img_cropped)
 
+plt.imsave('exports/6x6_calculated_pattern_plot.pdf', dpi = 300)
 
 
