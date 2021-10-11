bcf990107872ec437367597c77126225abfd169a
Marijn Venderbosch <48382744+MarijnVenderbosch@users.noreply.github.com>
2021-10-11 18:16:11 +0200

    Update look_up_table.py
    convert y scale to voltage

--- calibration/look_up_table.py
+++ calibration/look_up_table.py
@@ -13,18 +13,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #%% read data
-#df_813 = pd.read_csv('lut files/slm5953_at785_HDMI.lut', delimiter = ' ', header = None)
-#lut_813 = np.array(df_813)
 
+# Factory supplied .lut, commented for now
+# df_813 = pd.read_csv('lut files/slm5953_at785_HDMI.lut', delimiter = ' ', header = None)
+# lut_813_mV = np.array(df_813)
+# lut_820 = lut_813_mV * 5 / 2**(12)
+
+# Own diffractive lut calibration file
 df_820 = pd.read_csv('lut files/lut820_diffractivecalibration.lut', delimiter = ' ', header = None)
-lut_820 = np.array(df_820)
- 
+lut_820_mV = np.array(df_820)
+
+# Convert bit values to V values. 0-5V in 4096 (2^12) steps
+lut_820 = lut_820_mV * 5 / 2**(12)
+
 #%%plotting
 #plt.plot(lut_813[:, 1], label = '813.lut')
-plt.plot(lut_820[:, 1], label = '820.lut')
+fig, ax = plt.subplots(figsize = (4,3))
+ax.plot(lut_820[:, 1], label = '820.lut')
 
-plt.grid()
-plt.xlabel('grey level 0-255')
-plt.ylabel('voltage response [a.u]')
-plt.legend()
-plt.savefig('lut values.png', dpi = 300)
+ax.grid()
+ax.set_xlabel('grey level 0-255')
+ax.set_ylabel('voltage response [a.u]')
+#plt.legend()
+
+#%% saving
+plt.savefig('lut_plot.pdf', dpi = 300, bbox_inches = 'tight')
