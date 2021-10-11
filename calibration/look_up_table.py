54e7df01cc45e03559994ef4208e5f9b356841b9
Marijn Venderbosch <48382744+MarijnVenderbosch@users.noreply.github.com>
2021-10-11 18:29:17 +0200

    Update look_up_table.py

--- calibration/look_up_table.py
+++ calibration/look_up_table.py
@@ -22,18 +22,28 @@ import matplotlib.pyplot as plt
 # Own diffractive lut calibration file
 df_820 = pd.read_csv('lut files/lut820_diffractivecalibration.lut', delimiter = ' ', header = None)
 lut_820_mV = np.array(df_820)
-
 # Convert bit values to V values. 0-5V in 4096 (2^12) steps
 lut_820 = lut_820_mV * 5 / 2**(12)
 
+# Raw data of calibration, power in first order measurement
+df_raw_data = pd.read_csv('raw calibration data/Raw0.csv', delimiter = ',', header = None)
+raw_array = np.array(df_raw_data)
+raw_normalized = raw_array[:, 1] / np.max(raw_array[:, 1])
+
 #%%plotting
-#plt.plot(lut_813[:, 1], label = '813.lut')
+#ax.plot(lut_813[:, 1], label = '813.lut')
 fig, ax = plt.subplots(figsize = (4,3))
-ax.plot(lut_820[:, 1], label = '820.lut')
+ax.plot(lut_820[:, 1], label = '820.lut', color = 'blue')
 
 ax.grid()
 ax.set_xlabel('grey level 0-255')
-ax.set_ylabel('voltage response [a.u]')
+ax.set_ylabel('voltage response [V]', color = 'blue')
+
+# raw data
+ax2 = ax.twinx()
+ax2.plot(raw_normalized, color = 'red')
+ax2.set_ylabel(r'$P/P_0$', color = 'red')
+
 #plt.legend()
 
 #%% saving
