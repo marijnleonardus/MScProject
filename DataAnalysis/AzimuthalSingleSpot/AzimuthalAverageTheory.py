2fd04c272e61cd3fe74a57ff0ae0551bd317dc31
Marijn Venderbosch <marijnvenderbosch@gmail.com>
2021-12-01 15:21:48 +0100

    make script more readable
    renamed files as well

--- DataAnalysis/AzimuthalSingleSpot/AzimuthalAverageTheory.py
+++ DataAnalysis/AzimuthalSingleSpot/AzimuthalAverageTheory.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 10 10:41:07 2021
@@ -6,33 +7,43 @@ Created on Tue Aug 10 10:41:07 2021
 
 This script will compute the azimuthal average around a spot.
 Preferably a single spot so the rings are clearly visible. 
+
 """
 
-#%% imports
+#%% Imports
+
 import numpy as np
 import scipy.io
 from skimage.feature import blob_log
 import matplotlib.pyplot as plt
 from scipy.special import jv
 
-#%%variables
-""""This windows contains the variables that need to be edited, rest of script
-does not have to be edited"""
+#%% Variables
 
-# magnification from newport objective. This is uncalibrated. 
-magnification = 71.14
+#magnification from newport objective 
+magnification = 71.14 
+
+# Camera pixel size
+pixel_microns = 4.65
+
 # Threshold for LoG detection
 threshold = 0.2
+
 # Number of spots expected for LoG. Since we don't use a pattern of spots set to 1
 number_spots_expected = 1
-# mat file
 
-# theory plot parameters
-w_i = 2e-3
-R = 2e-3
-lam = 820e-9
-k = 2*np.pi / lam
-f = 4e-3
+# mat file
+location = 'images/'
+file_name = '1x1.mat'
+
+# Parameters for theoretical plot
+waist = 2e-3
+aperture_radius = 2e-3
+wavelength = 820e-9
+wavenumber = 2*np.pi / wavelength
+focal_length = 4e-3
+
+#%% Functions
 
 """" The following function will take the .mat file and export a grayscale numpy array
 with similar dimensions as the accompanying screenshot"""
@@ -53,25 +64,31 @@ def load_and_save(mat_file):
     cam_y_max = int(mat_file['cam_y_max'])
     
     # Cropping the array using the provided coordinates
-    cam_frame_cropped = np.transpose(cam_frame[cam_x_min : cam_x_max , cam_y_min : cam_y_max])
+    cam_frame_cropped = np.transpose(cam_frame[cam_x_min : cam_x_max , 
+                                               cam_y_min : cam_y_max])
     return cam_frame_cropped
  
 # execute function. Insert in brackets the .mat filename
-image0 = load_and_save('files/goodone.mat')
+image_cropped = load_and_save(location + file_name)
  
 
 #%% spot detection
-"""The following part will detect maxima using the Laplacian of Gaussian algorithm"""
-# Load image from script 'loadmatSaveCamFrame.py'. 
-# Image is cropped to region of interest
-# Transpose because camera is rotated
 
-# Use LoG blog detection. 
-# Max_sigma is the max. standard deviation of the Gaussian kernel used. 
-# Num_sigma the number of intermediate steps in sigma.
-# Threshold determines how easily blobs are detected. 
+"""The following part will detect maxima using the Laplacian of Gaussian algorithm,
+it will plot the spots as well"""
+
 def spot_detection(image):
-    spots_LoG = blob_log(image, max_sigma = 30, num_sigma = 10, threshold = threshold)
+    
+    # Use LoG blog detection. 
+    # Max_sigma is the max. standard deviation of the Gaussian kernel used. 
+    # Num_sigma the number of intermediate steps in sigma.
+    # Threshold determines how easily blobs are detected. 
+    
+    spots_LoG = blob_log(image,
+                         max_sigma = 30,
+                         num_sigma = 10, 
+                         threshold = threshold
+                         )
 
     # Check if expected amount of spots is detected
 
@@ -85,37 +102,51 @@ def spot_detection(image):
     # Find maxima locations and sizes. x and y are swapped becaues tranposed
     maxima_y_coordinates = spots_LoG[:, 0]
     maxima_x_coordinates = spots_LoG[:, 1]
+    
     # Increase sizes crosses for better visibility
     factor = 5
     sizes = spots_LoG[:, 2] * factor
 
     # Initialize plot
-    fig, axes = plt.subplots(1, 1, figsize=(5, 4))
+    fig, axes = plt.subplots(1, 1, figsize = (4, 3))
 
     # Plot original image and overlay with crosses on spots where blobs are detected
     # Radii or cicles are from the gaussian kernels that detected them
-    axes.set_title('Laplacian of Gaussian Spots')
+    axes.imshow(image)
+    
+    # Marker
+    axes.scatter(maxima_x_coordinates,
+                 maxima_y_coordinates, marker = 'x',
+                 s = sizes, 
+                 color = 'r',
+                 linewidth = 1
+                 )
+    
+    # Edits
     axes.set_xlabel('Pixels')
     axes.set_ylabel('Pixels')
-    axes.imshow(image)
-    axes.scatter(maxima_x_coordinates , maxima_y_coordinates, marker = 'x', s = sizes, color = 'r', linewidth = 1)
     
     # return for next script
     maximum_locs = np.array([maxima_x_coordinates, maxima_y_coordinates])
     return maximum_locs
 
 # Execute function and save maxima parameters
-maxima0 = spot_detection(image0)
+maxima_locations = spot_detection(image_cropped)
 
 
 #%% azimuthal average
 """"radial profile
 We want to compute the azimuthal average"""
 
-# We need an array to keep track of how many pixels we are from the center
-rad = np.arange(1, 101, 1)
-# convert from pixels to um using magnification
-rad_m = rad *10**(-6) * 4.65 / magnification
+def radial_distance(image, maxima):
+    # We need an array to keep track of how many pixels we are from the center
+    radial_distance_pixels = np.arange(1, 101, 1)
+    
+    # convert from pixels to um using magnification
+    radial_distance = radial_distance_pixels * 1e-6 * pixel_microns / magnification
+    return radial_distance_pixels, radial_distance
+
+radial_distance_pixels, radial_distance = radial_distance(image_cropped, maxima_locations)
 
 def azimuthal_average(image, maxima):
     # Image dimensions are 'a x b' 
@@ -123,11 +154,13 @@ def azimuthal_average(image, maxima):
     b = image.shape[1]
     
     # Make new coordinate system centered around the maximum and compute radii from center
-    [XX, YX] = np.meshgrid(np.arange(b) - maxima[0], np.arange(a) - maxima[1])
+    [XX, YX] = np.meshgrid(np.arange(b) - maxima[0]
+                           , np.arange(a) - maxima[1])
+    
     R = np.sqrt(XX**2 + YX**2)
     
     # We initialize an empty matrix that will store intensity on each ring
-    intensity = np.zeros(len(rad))
+    intensity_matrix = np.zeros(len(radial_distance))
     
     # How thick the rings are (the mask). Increase for more averaging and data points. 
     bin_size = 1
@@ -136,16 +169,17 @@ def azimuthal_average(image, maxima):
     # For each ring/mask, select only those pixels that are within the ring. 
     # And compute average intensity on the ring
     index = 0
-    for m in rad:
+    
+    for m in radial_distance_pixels:
         ring = (np.greater(R, m - bin_size) & np.less(R, m + bin_size))
         pixels_inside = image[ring]
-        intensity[index] = np.mean(pixels_inside)
+        intensity_matrix[index] = np.mean(pixels_inside)
         index +=1
     
-    intensity_normalized = intensity / np.max(intensity)
+    intensity_normalized = intensity_matrix / np.max(intensity_matrix)
     return intensity_normalized
 
-measurement0 = azimuthal_average(image0, maxima0)
+measurement = azimuthal_average(image_cropped, maxima_locations)
 
 
 #%% numerical integration, theory calculation
@@ -156,48 +190,79 @@ tweezer_matrix = []
 
 # Tweezer integral: r' is r_prime
 def tweezer(r_prime):
-    integral = np.exp(-r_prime**2 / w_i**2) * jv(0, k * r_prime * rad_m[i] / f) * r_prime
+    prefactor = np.exp(-r_prime**2 / waist**2) 
+    integral = prefactor * jv(0, wavenumber * r_prime * radial_distance[i] / focal_length) * r_prime
     return integral
 
 # Compute numerical integral as a function of r', save result by appending list
-for i in range(len(rad_m)):
-    result , error = scipy.integrate.fixed_quad(tweezer, 0, w_i)
+
+for i in range(len(radial_distance)):
+    result , error = scipy.integrate.fixed_quad(tweezer, 0, waist)
     tweezer_matrix.append(result)
 
-# Convert to np array, compute intensity, normalize
-tweezer_array = np.array(tweezer_matrix)    
-tweezer_intensity = abs(tweezer_array)**2
-tweezer_intensity_normalized = tweezer_intensity / np.max(tweezer_intensity)
+def tweezer_intensity(tweezer_matrix):
+    
+    # Convert to np array
+    tweezer_array = np.array(tweezer_matrix)   
+    
+    # Calculate intensity
+    tweezer_intensity = abs(tweezer_array)**2
+    
+    # Normalize
+    tweezer_intensity_normalized = tweezer_intensity / np.max(tweezer_intensity)
+    return tweezer_intensity_normalized
 
+tweezer_intensity = tweezer_intensity(tweezer_matrix)
 
-# Airy 
-airy = f / k / rad_m * jv(1, k * rad_m * R / f)
-airy_intensity = abs(airy)**2
-airy_intensity_normalized = airy_intensity / np.max(airy_intensity)
+def airy(f, k, radial_distance, R):
+    
+    # Definition Airy disk
+    airy = f / k / radial_distance * jv(1, k * radial_distance * R / f)
+    
+    # Calculate intensity
+    airy_intensity = abs(airy)**2
+    
+    # Normalize
+    airy_intensity_normalized = airy_intensity / np.max(airy_intensity)
+    return airy_intensity_normalized
+
+airy_intensity = airy(focal_length, wavenumber, radial_distance, aperture_radius)
     
 #%% plotting, saving
-# Plot tweezer
-fig, ax = plt.subplots(1,1, figsize = (4, 3))
+
+fig, ax = plt.subplots(1,1, figsize = (3.5, 2.5))
 ax.grid()
 
 # rescale x axis to show um instead of m
-rad_microns = rad_m * 1e6
+radial_distance_microns = radial_distance * 1e6
 
-ax.plot(rad_microns, tweezer_intensity_normalized, label = r'$w_i \approx R$')
-ax.scatter(rad_microns, measurement0,
+# Plot tweezer theory result
+
+ax.plot(radial_distance_microns,
+        tweezer_intensity,
+        label = r'$w_i \approx R$')
+
+# Plot measurement result
+
+ax.scatter(radial_distance_microns, measurement,
            label = 'measurement', 
            color = 'red', 
            s = 7,
            marker ='X'
            )
-ax.plot(rad_microns, airy_intensity_normalized, label = 'point spread function')
 
-ax.set_xlabel(r'radial coordinate in focal plane $r$ [$\mu$m]')
-ax.set_ylabel('normalized intensity [a.u.]')
+# Plot Airy theory result
+
+ax.plot(radial_distance_microns, airy_intensity, label = 'point spread function')
+
+ax.set_xlabel(r'$r$ [$\mu$m]', usetex = True)
+ax.set_ylabel(r'$I/I_0$', usetex = True)
 ax.legend()
 ax.set_xlim(0, 2)
 
 # Saving
-plt.savefig('theory_vs_measurement.pdf',
+
+plt.savefig('exports/AzimuthalAverage.pdf',
             dpi = 200,
+            pad_inches = 0,
             bbox_inches = 'tight')
